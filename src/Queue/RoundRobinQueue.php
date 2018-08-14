276fffcaed828b81b85fca864076aa437b55bf0e
Ben Davies <ben.davies@gmail.com>
2018-08-14 10:03:20 +0100

    fix performance issue with persistent queues

--- src/Queue/RoundRobinQueue.php
+++ src/Queue/RoundRobinQueue.php
@@ -57,7 +57,7 @@ class RoundRobinQueue implements Queue
 
         while (count($checked) < count($this->queues)) {
             $queue = current($this->queues);
-            $envelope = $queue->dequeue();
+            $envelope = $queue->dequeue(0);
             if (false === next($this->queues)) {
                 reset($this->queues);
             }
@@ -67,6 +67,8 @@ class RoundRobinQueue implements Queue
             } else {
                 $checked[] = $queue;
             }
+
+            usleep(10000);
         }
 
         return $envelope;
